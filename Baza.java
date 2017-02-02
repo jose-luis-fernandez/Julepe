b369b1026ff00f2a102e3a43f56a8c68fc6ad9ab
Jose Luis Fernandez <fernandezjl25@gmail.com>
2017-02-02 10:55:51 +0100

    clase baza añadida

---
+++ Baza.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Baza here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Baza
+{
+    // instance variables - replace the example below with your own
+    private int jugadores;
+    
+    private int paloQuePinta;
+    
+    
+    
+    /**
+     * Constructor for objects of class Baza
+     */
+    public Baza(int numeroJugadoresQueTiranCarta, int paloQuePinta)
+    {
+        // initialise instance variables
+        jugadores = numeroJugadoresQueTiranCarta;
+        this.paloQuePinta = paloQuePinta;
+    }
+
+    
+}
