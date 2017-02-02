8e65f2e554f76193c55683dac52078e78269f43c
Jose Luis Fernandez <fernandezjl25@gmail.com>
2017-02-02 13:14:07 +0100

    clase baza metodo nombreJugadorQueVaGanandoLaBaza añadido

--- Baza.java
+++ Baza.java
@@ -14,6 +14,10 @@ public class Baza
     
     private ArrayList<Carta> baza;
     
+    private ArrayList<String> nombresJugadores;
+    
+    private String jugadorQueVaGanando;
+    
     /**
      * Constructor for objects of class Baza
      */
@@ -23,11 +27,14 @@ public class Baza
         jugadores = numeroJugadoresQueTiranCarta;
         this.paloQuePinta = paloQuePinta;
         baza = new ArrayList<Carta>();
+        nombresJugadores = new ArrayList<String>();
+        jugadorQueVaGanando = null;
     }
     
     public void addCarta(Carta cartaTirada, String nombreJugadorQueTira)
     {
         baza.add(cartaTirada);
+        nombresJugadores.add(nombreJugadorQueTira);
     }
     
     public int getPaloPrimeraCartaDeLaBaza()
@@ -42,19 +49,34 @@ public class Baza
     public Carta cartaQueVaGanandoLaBaza()
     {
         Carta cartaADevolver = null;
-        if (baza.size() > 0){
+        if (baza.size() > 1){
             for (int i = 0; i < (baza.size()) - 1; i++){
                 if (baza.get(i).ganaA2(baza.get(i + 1), this.paloQuePinta)){
                     cartaADevolver = baza.get(i);
+                    jugadorQueVaGanando = nombresJugadores.get(i);
                 }
                 else {
                     cartaADevolver = baza.get(i + 1);
+                    jugadorQueVaGanando = nombresJugadores.get(i + 1);
                 }
             }
         }
+        else if (baza.size() == 1){
+            cartaADevolver = baza.get(0);
+            jugadorQueVaGanando = nombresJugadores.get(0);
+        }
         return cartaADevolver;
     }
-
+    
+    public String nombreJugadorQueVaGanandoLaBaza()
+    {
+        cartaQueVaGanandoLaBaza();
+        String jugadorQueVaGanando = null;
+        if (baza.size() > 0){
+            jugadorQueVaGanando = this.jugadorQueVaGanando;
+        }
+        return jugadorQueVaGanando;
+    }
     
     
     
